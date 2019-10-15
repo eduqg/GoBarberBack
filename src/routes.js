7012e7d48d99d6f5e9352fb1879462e614913da4
eduqg <eduardoqgomes@gmail.com>
2019-10-15 20:57:10 -0300

    User controller to post one user

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 import User from './app/models/User';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Eduardo',
-    email: 'edu@edu.com',
-    password_hash: '1092312129',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
