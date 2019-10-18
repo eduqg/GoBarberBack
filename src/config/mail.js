ee32e4fea045850fe02ed39c5d336239db7cd894
eduqg <eduardoqgomes@gmail.com>
2019-10-18 15:05:24 -0300

    Sending simple email

---
+++ src/config/mail.js
@@ -0,0 +1,22 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "c1b96f455a9e7f",
+    pass: "5130afd03d26a3"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/*
+Amazon SES
+Mailgun
+Sparkpost
+Mandril (Mailshimp)
+Gmail -> Limite de email
+
+Mailtrap (Para dev)
+*/
