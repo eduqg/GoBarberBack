df6def6b6864d499b780429991d008772c2ae2f0
eduqg <eduardoqgomes@gmail.com>
2019-10-18 11:56:39 -0300

    Verifying past dates and availability of appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -24,7 +25,28 @@ class AppointmentController {
       // 401 = Não autorizado
       return res.status(401).json({ error: 'You can only create appointments with providers' });
     }
-    console.log('UserID', req.userId);
+
+    // Verifica datas passadas
+    // ParseISO = transforma string do formato date
+    // StartOfHour = ao invés de 19:27, 19:00
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    // Verificar se pretador de serviço já não tem algo agendado para aquela hora
+    // Verifica se pretador está disponível
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date is not avaible' });
+    }
 
     const appointment = await Appointment.create({
       user_id: req.userId,
